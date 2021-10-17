a8294b9f96a6571a906c475234446531ce193d1a
DEPLOYER <admin@venty.cz>
2021-10-17 23:35:21 +0200

    CI - release script

---
+++ .github/workflows/docker/release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git checkout binaries
+git rm -f *.node
+cp dist/*.node . && git add *.node
+cp dist/uws.js . && git add uws.js
+git status
+git pull origin binaries
+git rev-parse master > source_commit && git add source_commit
+git checkout master docs/index.d.ts && /usr/bin/mv docs/index.d.ts . && git add -A docs/ && git add ./index.d.ts
+git clean -fd
+VERSION=$(curl -s https://api.github.com/repos/uNetworking/uWebSockets.js/releases/latest | jq -r '.name')
+echo $VERSION
+git config --global user.email "admin@venty.cz"
+git config --global user.name "VentyCZ"
+git commit -m "Update to release $VERSION"
+UVERSION="$VERSION-C7-TEST"
+git tag $UVERSION
+#git push "https://VentyCZ:$TOKEN@github.com/hotel-cz/uWebSockets.js" binaries
+git push "https://VentyCZ:$TOKEN@github.com/hotel-cz/uWebSockets.js" $UVERSION
